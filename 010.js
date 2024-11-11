f6efb4711f79ec76c41179f20dbde3ca4b3bc2aa
Tuugii <namjildorjtuguldur1234@gmail.com>
2024-11-11 23:28:15 +0800

    how to implement binary search

--- 010.js
+++ 010.js
@@ -1,15 +1,36 @@
 function binarySearch(searchList, value) {
-    let arrayPath = [];
-    let isThere=false
-    let pivot= Math.floor((searchList.length-1)/2)
-    while(!isThere)
+  let arrayPath = [];
+  let isThere=false
+  let low=0
+  let high=searchList.length-1
+  let pivot= Math.floor((low+high)/2)
+  while(true)
+  {
+    arrayPath.push(searchList[pivot])
+     if(low==high)
     {
-      arrayPath.push(searchList[pivot])
-      if(searchList[pivot]==value)
-      {
-        isThere=true
-      }
-      
+      break
     }
+    if(searchList[pivot]==value)
+    {
+      isThere=true
+      break
+    }
+    else if(value<searchList[pivot])
+    {
+      high=pivot-1
+    }
+    else if(value>searchList)
+    {
+      low=pivot+1
+    }
+    pivot= Math.floor((low+high)/2)
+  }
+  console.log('array path:',arrayPath)
+  if(isThere)
+  {
     return arrayPath;
   }
+  return 'Value Not Found'
+}
+console.log(binarySearch([1,2,4,5],4))
