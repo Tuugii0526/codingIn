760bcd557df1bdd79002e2ad485d8fd90ce66edb
Tuugii <namjildorjtuguldur1234@gmail.com>
2024-12-26 19:51:23 +0800

    I think circular queue is almost done

--- exp.js
+++ exp.js
@@ -17,7 +17,9 @@ class CircularQueue {
 
   enqueue(item) {
     // Only change code below this line
-    if (!this.queue[this.write + 1]) {
+    if (this.queue[this.write] == null) {
+      this.queue[this.write] = item;
+      this.write = (this.write + 1) % (this.max + 1);
     } else {
       return null;
     }
@@ -26,7 +28,7 @@ class CircularQueue {
 
   dequeue() {
     // Only change code below this line
-    if (this.queue[this.read]) {
+    if (this.queue[this.read] !== null) {
       const readItem = this.queue[this.read];
       this.queue[this.read] = null;
       this.read = (this.read + 1) % (this.max + 1);
@@ -47,8 +49,20 @@ console.log(q.print());
 console.log(q.enqueue(21));
 console.log(q.print());
 
-console.log(q.enqueue(21));
+console.log(q.enqueue(24));
 console.log(q.print());
 
+console.log(q.dequeue());
+console.log(q.print());
+
+console.log(q.dequeue());
+console.log(q.print());
+
+console.log(q.dequeue());
+console.log(q.print());
+console.log(q.enqueue(100));
+console.log(q.print());
+console.log(q.enqueue(25));
+console.log(q.print());
 console.log("queue read:", q.read);
 console.log("queue write", q.write);
